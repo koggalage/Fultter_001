57ecede4f06eeb69378e64310c7a668ba2ac3767
thilinacz <83072773+thilinacz@users.noreply.github.com>
2024-12-03 00:51:20 +0530

    #Added assets

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,24 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'My Body!',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-          ),),
+        child: Image.asset('assets/test.jpg')
+        // child: Image.network('assets/test.jpg')
+        //Image(
+            //image: NetworkImage('https://images.unsplash.com/photo-1733130778428-fd82429af975?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyN3x8fGVufDB8fHx8fA%3D%3D')
+            //image: AssetImage('assets/test.jpg')
+        //),
+
+        // Text(
+        //   'My Body!',
+        //   style: TextStyle(
+        //       fontSize: 20.0,
+        //       fontWeight: FontWeight.bold,
+        //       letterSpacing: 2.0,
+        //       color: Colors.grey[600],
+        //       fontFamily: 'IndieFlower'
+        //   ),
+        // ),
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: AboutDialog.new,
