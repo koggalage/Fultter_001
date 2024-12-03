c3be0230ec67ffe88b7f8faf30deca6b75b08bb0
thilinacz <83072773+thilinacz@users.noreply.github.com>
2024-12-03 22:32:45 +0530

    #Buttons and Icons

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ void main() => runApp(MaterialApp(
 
 //stless tab
 class Home extends StatelessWidget {
+  const Home({super.key});
+
  // const ({super.key});
 
   @override
@@ -17,7 +19,27 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset('assets/test.jpg')
+        child: IconButton(
+            color: Colors.blue,
+            onPressed: () {
+             // print('IconButton pressed!'); // Prints text to console
+            },
+            icon: const Icon(Icons.alternate_email))
+        // child: TextButton.icon(
+        //   onPressed: () {
+        //   },
+        //   icon: const Icon(
+        //     Icons.mail
+        //   ),
+        //   label: const Text('Mail Me!'),
+        //   //child: const Text('Click Me!')
+        // ),
+        // child: Icon(
+        //     Icons.airport_shuttle,
+        //     color: Colors.lightBlue,
+        //     size: 100.0
+        // ),
+        //child: Image.asset('assets/test.jpg')
         // child: Image.network('assets/test.jpg')
         //Image(
             //image: NetworkImage('https://images.unsplash.com/photo-1733130778428-fd82429af975?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyN3x8fGVufDB8fHx8fA%3D%3D')
